63f9052eb9b6866cd43dcd5eb159bf7311fce37f
arnau <arnau.mayoral10@gmail.com>
2024-03-18 11:32:09 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_safety',
       version='0.0.1',
-      install_requires=['gym']#And any other dependencies required
+      install_requires=['gym'],#And any other dependencies required
+      packages=find_packages(),  # And any other packages required
 )
